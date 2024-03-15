cc91e6afb4809bb090ba139c9067b2927be23fc5
Alakh shukla <alakhshukla13@gmail.com>
2024-03-15 13:01:20 +0530

    CRUD-pannel

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+import "./App.css";
+import { Table } from "./components/Table";
+import { Modal } from "./components/Modal";
+
+function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [rows, setRows] = useState([
+    {
+      Product: "Laptop",
+      description: "Electronic Gadget",
+      status: "Available",
+    },
+    {
+        Product: "About Us",
+      description: "This page has details about the company",
+      status: "Limited",
+    },
+    {
+        Product: "Pricing",
+      description: "Prices for different subscriptions",
+      status: "Unavailable",
+    },
+  ]);
+  const [rowToEdit, setRowToEdit] = useState(null);
+
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+
+  const handleEditRow = (idx) => {
+    setRowToEdit(idx);
+
+    setModalOpen(true);
+  };
+
+  const handleSubmit = (newRow) => {
+    rowToEdit === null
+      ? setRows([...rows, newRow])
+      : setRows(
+          rows.map((currRow, idx) => {
+            if (idx !== rowToEdit) return currRow;
+
+            return newRow;
+          })
+        );
+  };
+
+  return (
+    <div className="App">
+      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+      <button onClick={() => setModalOpen(true)} className="btn">
+        Add
+      </button>
+      {modalOpen && (
+        <Modal
+          closeModal={() => {
+            setModalOpen(false);
+            setRowToEdit(null);
+          }}
+          onSubmit={handleSubmit}
+          defaultValue={rowToEdit !== null && rows[rowToEdit]}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import { BsFillTrashFill, BsFillPencilFill } from "react-icons/bs";
+
+import "./Table.css";
+
+export const Table = ({ rows, deleteRow, editRow }) => {
+  return (
+    <div className="table-wrapper">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th className="expand">Description</th>
+            <th className="status">Status</th>
+            <th className="actions">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row, idx) => {
+            const statusText =
+              row.status.charAt(0).toUpperCase() + row.status.slice(1);
+
+            return (
+              <tr key={idx}>
+                <td>{row.Product}</td>
+                <td className="expand">{row.description}</td>
+                <td>
+                  <span className={`label label-${row.status} status`}>
+                    {statusText}
+                  </span>
+                </td>
+                <td className="fit">
+                  <span className="actions">
+                    <BsFillTrashFill
+                      className="delete-btn"
+                      onClick={() => deleteRow(idx)}
+                    />
+                    <BsFillPencilFill
+                      className="edit-btn"
+                      onClick={() => editRow(idx)}
+                    />
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
